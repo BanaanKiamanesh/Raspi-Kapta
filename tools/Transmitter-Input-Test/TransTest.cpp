8828a39971fcc94d9611e881c6bc5fef15d683b8
BanaanKiamanesh <kiamaneshbanaan@gmail.com>
2022-07-23 06:59:19 +0100

    Transmitter Testing Tool Added

---
+++ tools/Transmitter-Input-Test/TransTest.cpp
@@ -0,0 +1,23 @@
+#include "../../include/RECEIVER.h"
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+using namespace std;
+
+int main()
+{
+    Receiver rx;
+
+    while (1)
+    {
+        rx.updateData();
+
+        std::cout << "Pitch : " << rx.channel(1);
+        std::cout << " - Roll : " << rx.channel(2);
+        std::cout << " - Throttle : " << rx.channel(3);
+        std::cout << " - Yaw : " << rx.channel(4) << "\n";
+    }
+
+    return 0;
+}
